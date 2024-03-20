683bad8a359500982b79dd31f7139d3a2c38f6b2
efficient75@icloud.com <efficient75@icloud.com>
2024-03-20 12:47:12 +0100

    Setting up the filter and injecting HTML, modifying the CSS, adding animation

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,3 +1,24 @@
+const filtersHTML = `
+    <div class="filters">
+        <button class="btn-filter">Tous</button>
+        <button class="btn-filter">Objets</button>
+        <button class="btn-filter">Appartements</button>
+        <button class="btn-filter">Hotels &amp; restaurants</button>
+    </div>
+`;
+
+// Sélectionnez la balise <section> avec l'ID "portfolio"
+const portfolioSection = document.getElementById('portfolio');
+
+// Insérez les boutons de filtre juste avant le titre "Mes Projets"
+portfolioSection.insertAdjacentHTML('beforebegin', filtersHTML);
+
+// Déclarer la variable works au niveau global
+let works = [];
+
+// Appel de la fonction fetchData
+fetchData();
+
 // Récupération des works depuis API
 async function fetchData() {
     try {
@@ -35,5 +56,47 @@ async function fetchData() {
     }
 }
 
-// Appel de la fonction fetchData
-fetchData();
+
+/**************************ZONE FILTRE******************************************/
+// Ajouter des écouteurs d'événements à chaque bouton de filtre
+document.querySelectorAll('.btn-filter').forEach(button => {
+    button.addEventListener('click', () => {
+        filterWorks(button.textContent.trim()); // Utilisez le texte du bouton comme critère de filtrage
+    });
+});
+
+// Définir une fonction de filtrage
+function filterWorks(filter) {
+    const sectionGallery = document.querySelector('.gallery');
+    const filteredWorks = works.filter(project => {
+        // Vérifiez si le projet correspond au filtre
+        if (filter === 'Tous') {
+            return true; // Afficher tous les projets
+        } else {
+            // Vérifiez si le projet appartient à la catégorie correspondant au filtre
+            return project.category === filter;
+        }
+    });
+
+    // Afficher les projets filtrés
+    if (filteredWorks.length > 0) {
+        // Effacer le contenu précédent de la galerie
+        sectionGallery.innerHTML = '';
+
+        // Utiliser innerHTML pour injecter le HTML pour chaque projet filtré
+        filteredWorks.forEach(project => {
+            sectionGallery.innerHTML += `
+                <figure>
+                    <img src="${project.imageUrl ?? '(Pas d\'image)'}" alt="${project.title ?? '(pas de title)'}">
+                    <figcaption>${project.title ?? '(pas de title)'}</figcaption>
+                </figure>
+            `;
+        });
+    } else {
+        console.error('Aucun projet trouvé pour ce filtre');
+        // Vous pouvez afficher un message d'erreur ou un message indiquant qu'aucun projet n'est trouvé pour ce filtre
+    }
+}
+
+
+
